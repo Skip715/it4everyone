002c8d0583e3559d317b0ca0c30baf8aac0b252d
Skip715 <lisaskipton78@gmail.com>
2026-06-18 13:04:24 +0100

    Add files via upload

---
+++ deploy/deploy 2/app/page.tsx
@@ -0,0 +1,22 @@
+import Hero from '@/components/Hero'
+import { TrustBar, BrandsBar } from '@/components/TrustBar'
+import ProductGrid from '@/components/ProductGrid'
+import ServicesSection from '@/components/ServicesSection'
+import B2BSection from '@/components/B2BSection'
+import ContactSection from '@/components/ContactSection'
+import WhyUsSection from '@/components/WhyUsSection'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <TrustBar />
+      <BrandsBar />
+      <ProductGrid />
+      <ServicesSection />
+      <WhyUsSection />
+      <B2BSection />
+      <ContactSection />
+    </>
+  )
+}
